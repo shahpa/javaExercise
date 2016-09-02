edc6af58b8b589074ef78e8d0bbdf254ea4e645a
shahpa <shah.jparth@gmail.com>
2016-09-01 23:59:00 -0700

    findMedian and wordLadder -- partial implementation

---
+++ src/githubPractices/WordLadder.java
@@ -0,0 +1,21 @@
+package githubPractices;
+
+import java.util.*;
+
+public class WordLadder {
+
+	public static void main(String[] args) {
+		String start = "hit";
+		String end = "cog";
+		String[] distArray = {"hot","dot","dog","lot","log"};
+		Set<String> dist = new HashSet<String>();
+		
+		for(int i = 0; i< distArray.length; i++)  {
+			dist.add(distArray[i]);
+		}
+		
+		
+
+	}
+
+}
