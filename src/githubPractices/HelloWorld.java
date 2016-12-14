ea51fe3346737bc3acb3141ca66e27e6edf7c3fd
shahpa <shah.jparth@gmail.com>
2016-12-13 18:42:24 -0800

    test

--- src/githubPractices/HelloWorld.java
+++ src/githubPractices/HelloWorld.java
@@ -1,5 +1,7 @@
 package githubPractices;
 
+import java.util.Hashtable;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
@@ -7,9 +9,44 @@ public class HelloWorld {
 		System.out.println("Hello World !");
 		
 		//bubble sort example
-				int[] inputArray = {10, 9, 8, 7, 6, 5};
+				/*int[] inputArray = {10, 9, 8, 7, 6, 5};
 				BubbleSort bubbleSortObj = new BubbleSort();
-				bubbleSortObj.doBubbleSort(inputArray);
+				bubbleSortObj.doBubbleSort(inputArray);*/
+				
+				/*int result = wordpattern("abb", null);
+				System.out.println(result);
+				*/
+				MultiThreading mt = new MultiThreading();
+				mt.testThread();
+				
+				
 	}
+	
+	static int wordpattern(String pattern, String input) {
+        Hashtable<Character, String> dict = new Hashtable<Character, String>();
+        dict.put('a', "red");
+        dict.put('b', "blue");
+        
+        String result = "";
+        
+        if(pattern == null)  {
+            return 0;
+        }
+        if(input == null)  {
+            return 0;
+        }
+        for(int i=0; i< pattern.length(); i++)  {
+            if(dict.containsKey(pattern.charAt(i)))  {
+                result = result + dict.get(pattern.charAt(i));
+            }
+        }
+        if(input.equals(result))  {
+            return 1;
+        }
+        else  {
+            return 0;
+        }
+
+    }
 
 }
