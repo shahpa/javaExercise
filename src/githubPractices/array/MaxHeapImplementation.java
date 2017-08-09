b8198e5eb6be5d2e017ef9daee1f02540adb8f63
Parth_Shah <Parth_Shah@W2519800BC9LX.SYMC.SYMANTEC.COM>
2017-08-09 00:08:03 -0700

    MaxHeap array implementation

---
+++ src/githubPractices/array/MaxHeapImplementation.java
@@ -0,0 +1,19 @@
+package githubPractices.array;
+
+public class MaxHeapImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MaxHeap heap = new MaxHeap();
+		heap.insert(12);
+		heap.insert(7);
+		heap.insert(6);
+		heap.insert(10);
+		heap.insert(8);
+		heap.insert(20);
+		
+		heap.print();
+	}
+
+}
